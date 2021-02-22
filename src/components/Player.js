b17703c074c87330f621b3b210e1818a4d2aca4c
macfrei <lene@7freiheiten.de>
2021-02-22 12:53:26 +0100

    Rewrite player css to styled-component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,14 +1,26 @@
-import './Player.css'
+import styled from 'styled-components/macro'
 
 export default function Player({ name, score, onMinus, onPlus }) {
   return (
-    <section className="Player">
+    <PlayerWrapper>
       {name}
-      <div className="Player__score">
+      <PlayerScore>
         <button onClick={onMinus}>-</button>
         <span>{score}</span>
         <button onClick={onPlus}>+</button>
-      </div>
-    </section>
+      </PlayerScore>
+    </PlayerWrapper>
   )
 }
+
+const PlayerWrapper = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+const PlayerScore = styled.div`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(3, 1fr);
+  place-items: center;
+`
